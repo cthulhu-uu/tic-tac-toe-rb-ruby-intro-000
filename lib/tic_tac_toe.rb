ddd34fb6955254c4620e3687d30641fa1e7aad5e
Sky Resendez <sky.resendez@gmail.com>
2018-08-30 09:31:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, player_token)
-  board[index] = current_player
+  board[index] = player_token
 end
 
 def position_taken?(board, pos)
@@ -37,6 +37,21 @@ def player_token (board)
   return "O"
 end
 
+
+# END GAME SHIZZ
+
+
+WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle rows
+  [6,7,8], # bottom row
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # diagonal one
+  [2,4,6] # diagonal two
+]
+
 def won?(board)
   WIN_COMBINATIONS.each do |win|
     if board[win[0]] == "X" && board[win[1]] == "X" && board[win[2]] == "X"
@@ -48,18 +63,6 @@ def won?(board)
   end
     return false
   end
-
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2], # top row
-  [3,4,5], # middle rows
-  [6,7,8], # bottom row
-  [0,3,6], # left column
-  [1,4,7], # middle column
-  [2,5,8], # right column
-  [0,4,8], # diagonal one
-  [2,4,6] # diagonal two
-]
 
 def full?(board)
   return true unless board.include?(" ")
