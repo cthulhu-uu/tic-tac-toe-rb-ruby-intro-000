79b18e830a05db47c2899329b75d02f4b6dfccbc
Sky Resendez <sky.resendez@gmail.com>
2018-08-30 09:27:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,6 +20,23 @@ def position_taken?(board, pos)
   !(board[pos].nil? || board[index] == " ")
 end
 
+def turn_count(board)
+  count = 0
+  board.each do |pos|
+    if pos != " "
+      count +=1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  end
+  return "O"
+end
+
 def won?(board)
   WIN_COMBINATIONS.each do |win|
     if board[win[0]] == "X" && board[win[1]] == "X" && board[win[2]] == "X"
