3d5fa17431e3f0656809c0232eb48d1a876b8592
Sky Resendez <sky.resendez@gmail.com>
2018-08-30 09:18:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,21 @@
 
 # Helper Method
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
 def position_taken?(board, pos)
   !(board[pos].nil? || board[index] == " ")
 end
